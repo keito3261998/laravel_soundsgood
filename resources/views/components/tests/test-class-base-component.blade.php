d62f4ddfa796f74d3a1dbc6cafc6368d795fcad6
keito3261998 <keito3261998@gmail.com>
2022-05-23 12:48:32 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+		クラスベースのコンポーネントです。
+		<div>
+			{{ $classBaseMessage }}
+		</div>
+		<div>
+			{{ $defaultMessage }}
+		</div>
+</div>
