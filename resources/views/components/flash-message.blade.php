c4e42de8afdee945c786cae3ef2f23c386f8e283
keito3261998 <keito3261998@gmail.com>
2022-06-05 03:08:42 +0900

    edit, update

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -6,6 +6,6 @@ if($status === 'error'){$bgColor = 'bg-red-500';}
 @endphp
 
 @if(session('message'))
-<div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+<div class="{{ $bgColor }} lg:w-2/3 w-full mx-auto p-2 text-white mb-4">
 {{ session('message' )}} </div>
 @endif
