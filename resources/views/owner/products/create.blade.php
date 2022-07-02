982f38780a246f05df5103a10991ad2cb23ac89e
keito3261998 <keito3261998@gmail.com>
2022-07-03 00:28:16 +0900

    micromodal

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -19,7 +19,7 @@
 															@foreach($categories as $category)
 																<optgroup label="{{ $category->name }}">
 																@foreach($category->secondary as $secondary)
-																<option value="{{ $secondary->id}}"> 
+																<option value="{{ $secondary->id}}">
 																	{{ $secondary->name }}
 																</option>
 																@endforeach
@@ -27,6 +27,10 @@
 														</select>
                           </div>
                         </div>
+												<x-select-image :images="$images" name="image1" />
+												<x-select-image :images="$images" name="image2" />
+												<x-select-image :images="$images" name="image3" />
+												<x-select-image :images="$images" name="image4" />
 												<div class="p-2 w-1/2 mx-auto flex justify-between mt-4">
 													<button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="bg-gray-200 bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
 													<button type="submit" class="text-white bg-yellow-400 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-500 rounded text-lg">登録する</button>
@@ -38,4 +42,21 @@
             </div>
         </div>
     </div>
+<script>
+    'use strict'
+    const images = document.querySelectorAll('.image')
+
+    images.forEach( image =>  {
+      image.addEventListener('click', function(e){
+        const imageName = e.target.dataset.id.substr(0, 6)
+        const imageId = e.target.dataset.id.replace(imageName + '_', '')
+        const imageFile = e.target.dataset.file
+        const imagePath = e.target.dataset.path
+        const modal = e.target.dataset.modal
+        document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+        document.getElementById(imageName + '_hidden').value = imageId
+        MicroModal.close(modal);
+    }, )
+    })
+  </script>
 </x-app-layout>
